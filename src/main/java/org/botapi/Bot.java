fe5ca9b660a71404ac9b45de6494d21f37156d8f
Niunda <kav7107@gmail.com>
2024-07-02 23:22:57 +0300

    Initial commit

---
+++ src/main/java/org/botapi/Bot.java
@@ -0,0 +1,12 @@
+package org.botapi;
+
+
+import java.util.logging.Logger;
+
+public class Main {
+    private final Logger log = Logger.getLogger()
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
